a96def4d604290fa94867662b5f81ae559991804
Alchemy <maanoatu17@gmail.com>
2018-04-01 19:35:11 +0300

    adding credentials in main function

--- run.py
+++ run.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3.6
+import random
 from user import User
 from credentials import Credentials
-import random
 
 def create_users(name,password):
     '''
@@ -12,15 +12,15 @@ def create_users(name,password):
 
 def save_new_user(user):
     '''
-    Functions that appends a user to the credentials
+    Functions that appends a user to the list
     '''
-    User.save_user()
+    user.save_user()
 
-def account_login(login):
+def user_exist(name):
     '''
-    Function to login
+    Function that returns a boolean value based on whether the exists
     '''
-    return User.user_login(login)
+    return User.user_exists(name)
 
 def create_credentials(account_name, account, user_name, password):
     '''
@@ -33,13 +33,13 @@ def save_user_credential(credentials):
     '''
     Function to save credentials
     '''
-    credentials.save_credentials()
+    Credentials.save_credentials()
 
 def delete_credential(credentials):
     '''
     Function to delete credentials from list
     '''
-    credentials.delete_credentials()
+    Credentials.delete_credentials()
 
 def find_credentials(name):
     '''
@@ -52,4 +52,103 @@ def credentials_exists(name):
     Function that returns a boolean value based on whether the user 
     being sough exists
     '''
-    return Credentials.credential_exists(name)    
+    return Credentials.credential_exists(name)
+
+def display_credentials():
+    '''
+    Function that returns all the saved credentials
+    '''
+    return Credentials.display_credentials()
+
+def main():
+    print("Welcome to password locker")
+    while True:
+        print("  ___________________")
+        print(" |    su - sign up   | \n |    lg - login     | \n |    ex - exit      |")
+        print(" |___________________|")
+        short_code = input().lower()
+        if short_code == "su":
+            print("Input Username: ")
+            su_username = input()
+            print("Input Password: ")
+            su_password = input()
+            print('  ----------------------')
+
+            save_new_user(create_users(su_username, su_password))
+            print("\n | User Has Been Created | \n")
+            print('  ---------------------')
+            print("USER             PASSWORD")
+            print(f"{su_username}             {su_password} ")
+        elif short_code == "lg":
+            print("Input User Name:")
+            lg_username = input()
+            print("Input Password: ")
+            lg_password = input()
+
+            if user_exist(lg_username):
+                print(f"Welcome {lg_username}")
+                while True:
+                    print ('-'*10)
+                    print("cc :to add a credential")
+                    print("dc :to display a credential") 
+                    print("dl :to delete a credential")
+                    print("ex :to exit")
+                    short_code = input() 
+
+                    if short_code == 'cc':
+                        while True:
+                            print("1 : to insert password")
+                            print("2 : generate password")
+                            print("3 : back to main menu")
+                            short_code1 = input()
+
+                            if short_code1 == '1':
+                                print(f"WELCOME {credential_user_name}, ADD A CREDENTIAL") 
+                                print ('-'*10)
+                                print("ENTER CREDENTIAL NAME")
+                                name_of_credential =input()
+                                print("ENTER CREDENTIAL'S PASSWORD")
+                                password_of_credential = input()
+
+                                save_credential_run(create_credential(credential_user_name,name_of_credential,password_of_credential))
+                                
+                                print("YOUR CREDENTIALS")
+                                print ('-'*10)
+                                for credential in Credentials.display_all_credentials():
+                                    if credential_user_name == credential.user_name:
+                                        print(f"CREDENTIAL:{credential.credential_name}......PASSWORD:{credential.credential_password}")
+                                        print ("\n")
+                            
+
+                            elif short_code1 == '2':
+                                print ('-'*10)
+                                print(f"WELCOME {credential_user_name}, ADD A CREDENTIAL")
+                                print ('-'*10) 
+                                print("ENTER CREDENTIAL NAME")
+                                name_of_credential =input()
+                                print("YOUR PASSWORD HAS BEEN GENERATED")
+                                password_of_credential = random.randint(10000,99000)
+
+                                save_credential_run(create_credential(credential_user_name,name_of_credential,password_of_credential))
+                                
+                                print("YOUR CREDENTIALS")
+                                print ('-'*10)
+                                for credential in Credentials.display_all_credentials():
+                                    if credential_user_name == credential.user_name:
+                                        print(f"CREDENTIAL:{credential.credential_name}......PASSWORD:{credential.credential_password}")
+                                        print ("\n")
+
+                            elif short_code1 == 'ok':
+                                break
+
+                            elif short_code1 == 'ex':
+                                break
+                            else:
+                                    print("INVALID INPUT PLEASE USE THE PROVIDED SHORTCODES")
+                                    print ("\n")                                   
+            
+        
+
+
+if __name__ == '__main__':
+    main()
--- user.py
+++ user.py
@@ -14,5 +14,14 @@ class User:
         '''
         save_user method saves user objects into list_for_users
         '''
-        User.list_for_users.append(self)
+        self.list_for_users.append(self)
     
+    @classmethod
+    def user_exists(cls, username):
+        '''
+        A method that determines if a user exists
+        '''
+        for user in cls.list_for_users:
+            if user.username == username:
+                return True
+        return False
